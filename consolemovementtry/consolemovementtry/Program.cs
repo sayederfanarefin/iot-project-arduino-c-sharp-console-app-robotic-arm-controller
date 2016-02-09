e935047ed90c581fde054a0bc67be7f8dfdcd55c
Sayed Erfan Arefin <SayedErfan.Arefin@studentpartner.com>
2016-02-09 17:14:23 +0600

    motamoti line e ashchi
    i guess

--- consolemovementtry/consolemovementtry/Program.cs
+++ consolemovementtry/consolemovementtry/Program.cs
@@ -20,8 +20,6 @@ namespace consolemovementtry
             SerialPort port = new SerialPort(portName, 9600);
             port.Open();
            
-            
-
             while (true) { 
                 ConsoleKeyInfo keyInfo = Console.ReadKey();
             
@@ -37,16 +35,53 @@ namespace consolemovementtry
                 }
                 else if (keyInfo.Key == ConsoleKey.LeftArrow)
                 {
-                    Console.WriteLine("left");
+                    Console.WriteLine("claw close");
                     port.Write("2");
                 }
                 else if (keyInfo.Key == ConsoleKey.RightArrow)
                 {
-                    Console.WriteLine("right");
+                    Console.WriteLine("claw open");
                     port.Write("3");
                 }
+                else if (keyInfo.Key == ConsoleKey.B)
+                {
+                    //base
+                    Console.WriteLine("base");
+                    port.Write("4");
+                }
+                else if (keyInfo.Key == ConsoleKey.S)
+                {
+                    //shoulder
+                    Console.WriteLine("shoulder");
+                    port.Write("5");
+                }
+                else if (keyInfo.Key == ConsoleKey.W)
+                {
+                    //wrist up down
+                    Console.WriteLine("wrist updown");
+                    port.Write("6");
+                }
+                else if (keyInfo.Key == ConsoleKey.M)
+                {
+                    //wrist left
+                    Console.WriteLine("wrist left");
+                    port.Write("7");
+                }
+                else if (keyInfo.Key == ConsoleKey.N)
+                {
+                    //wrist right
+                    Console.WriteLine("wrist right");
+                    port.Write("8");
+                }
+                else if (keyInfo.Key == ConsoleKey.Escape)
+                {
+                    //wrist right
+                    Console.WriteLine("Good bye");
+                    System.Threading.Thread.Sleep(1000);
+                    break;
+                }
             }
-            Console.ReadLine();
+            //Console.ReadLine();
             port.Close();
         }
     }
