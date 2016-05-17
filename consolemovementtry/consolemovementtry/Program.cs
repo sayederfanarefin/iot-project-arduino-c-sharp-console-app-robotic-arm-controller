7db9bf7dcde65a190f38418ccf7cb43fb0b0ee93
Sayed Erfan Arefin <erfanjordison@gmail.com>
2016-05-17 23:44:27 +0600

    the desktop app is prepared

--- consolemovementtry/consolemovementtry/Program.cs
+++ consolemovementtry/consolemovementtry/Program.cs
@@ -19,59 +19,126 @@ namespace consolemovementtry
             String portName = Console.ReadLine();
             SerialPort port = new SerialPort(portName, 9600);
             port.Open();
-           
+
+            int base1 = 0;
+            int base2 = 180;
+            int shoulder = 0;
+            int arm = 0;
+            int wrist = 0;
+            int claw = 0;
+
+            /*
+            -1 = shit
+            0 = base
+            1 = shoulder 
+            2 = arm
+            */
+            int selected = -1;
+             
+
             while (true) { 
                 ConsoleKeyInfo keyInfo = Console.ReadKey();
             
                 if (keyInfo.Key == ConsoleKey.UpArrow)
                 {
-                    Console.WriteLine("up");
-                    port.Write("0");
+                    
+                    if (selected == 0)
+                    {
+                        
+                        base1++;
+                        base2--;
+                        Console.WriteLine("base" +base1.ToString());
+                    }
+                    else if (selected == 1)
+                    {
+                        shoulder++;
+                        Console.WriteLine("shoulder" + shoulder.ToString());
+                    }
+                    else if (selected == 2)
+                    {
+                        arm++;
+                        Console.WriteLine("arm" + arm.ToString());
+                    }
+                    else if (selected == -1)
+                    {
+                        Console.WriteLine("Nothing selected");
+                    }
+                    else
+                    {
+                        Console.WriteLine("unknown call :P");
+                    }
+                    
                 }
                 else if (keyInfo.Key == ConsoleKey.DownArrow)
                 {
-                    Console.WriteLine("down");
-                    port.Write("1");
+                    if (selected == 0)
+                    {
+
+                        base1--;
+                        base2++;
+                        Console.WriteLine("base" + base1.ToString());
+                    }
+                    else if (selected == 1)
+                    {
+                        shoulder--;
+                        Console.WriteLine("shoulder" + shoulder.ToString());
+                    }
+                    else if (selected == 2)
+                    {
+                        arm--;
+                        Console.WriteLine("arm" + arm.ToString());
+                    }
+                    else if (selected == -1)
+                    {
+                        Console.WriteLine("Nothing selected");
+                    }
+                    else
+                    {
+                        Console.WriteLine("unknown call :P");
+                    }
+
                 }
                 else if (keyInfo.Key == ConsoleKey.LeftArrow)
                 {
-                    Console.WriteLine("claw close");
-                    port.Write("2");
+                    claw--;
+                    Console.WriteLine("claw close "+ claw.ToString());
+                    
                 }
                 else if (keyInfo.Key == ConsoleKey.RightArrow)
                 {
-                    Console.WriteLine("claw open");
-                    port.Write("3");
+                    claw++;
+                    Console.WriteLine("claw open "+ claw.ToString());
+                   
                 }
                 else if (keyInfo.Key == ConsoleKey.B)
                 {
                     //base
                     Console.WriteLine("base");
-                    port.Write("4");
+                    selected = 0;
                 }
                 else if (keyInfo.Key == ConsoleKey.S)
                 {
                     //shoulder
                     Console.WriteLine("shoulder");
-                    port.Write("5");
+                    selected = 1;
                 }
                 else if (keyInfo.Key == ConsoleKey.W)
                 {
                     //wrist up down
-                    Console.WriteLine("wrist updown");
-                    port.Write("6");
+                    Console.WriteLine("arm");
+                    selected = 2;
                 }
                 else if (keyInfo.Key == ConsoleKey.M)
                 {
                     //wrist left
                     Console.WriteLine("wrist left");
-                    port.Write("7");
+                    wrist++;
                 }
                 else if (keyInfo.Key == ConsoleKey.N)
                 {
                     //wrist right
                     Console.WriteLine("wrist right");
-                    port.Write("8");
+                    wrist--;
                 }
                 else if (keyInfo.Key == ConsoleKey.Escape)
                 {
@@ -80,6 +147,16 @@ namespace consolemovementtry
                     System.Threading.Thread.Sleep(1000);
                     break;
                 }
+
+                
+                String return_ = base1.ToString() + "," + 
+                                 base2.ToString() + "," + 
+                                 shoulder.ToString() + "," +
+                                 arm.ToString() + "," +
+                                 wrist.ToString() + "," +
+                                 claw.ToString();
+
+                port.Write(return_);
             }
             //Console.ReadLine();
             port.Close();
